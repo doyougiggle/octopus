d9f5053e3b0158136c9762f48e8fd0355d7ff554
hectormartinez <hector.martinez.a@gmail.com>
2017-05-13 01:03:04 +0200

    scripts

---
+++ src/parse_all_train_en-fr.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+
+DIR=/projdata/alpage2/hmartine/data/discomt17/toparse/train/en-de
+target_mod=/projdata/alpage2/hmartine/tools/en_lemma_petrov.turbomodel_svm_mira/fr_train_lemma_petrov.conll.model
+source_mod=/projdata/alpage2/hmartine/tools/de_form_upos.turbomodel_svm_mira/en_train_form_upos.conll.model
+source_lang=en
+
+for file in `ls $DIR/*withids`
+do
+bash parse_target_lemmapetrov.sh $file $target_mod
+bash parse_source_formpos.sh $file $source_mod $source_lang
+done
--- src/parse_all_train_es-en.sh
+++ src/parse_all_train_es-en.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# base
+
 
 
 DIR=/projdata/alpage2/hmartine/data/discomt17/toparse/train/en-de
 target_mod=/projdata/alpage2/hmartine/tools/en_lemma_petrov.turbomodel_svm_mira/en_train_lemma_petrov.conll.model
-source_mod=/projdata/alpage2/hmartine/tools/de_form_upos.turbomodel_svm_mira/de_train_form_upos.conll.model
-source_lang=de
+source_mod=/projdata/alpage2/hmartine/tools/de_form_upos.turbomodel_svm_mira/es_train_form_upos.conll.model
+source_lang=es
 
 for file in `ls $DIR/*withids`
 do
 bash parse_target_lemmapetrov.sh $file $target_mod
 bash parse_source_formpos.sh $file $source_mod $source_lang
 done
-
