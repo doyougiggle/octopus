564aa370bff997915ed8926d1091ea99428adbc8
hectormartinez <hector.martinez.a@gmail.com>
2017-05-13 00:46:03 +0200

    rm

--- src/parse_source_formpos.sh
+++ src/parse_source_formpos.sh
@@ -12,5 +12,3 @@ cd /projdata/alpage2/hmartine/tools
 bash parse_with_turboparser.sh $file.src2 $model
 cd -
 python parsed_to_inline_notation.py --infile $file.src2.parsed --outfile $file.src.inline
-rm $file.src
-rm $file.src2
--- src/parse_target_lemmapetrov.sh
+++ src/parse_target_lemmapetrov.sh
@@ -7,4 +7,3 @@ cd /projdata/alpage2/hmartine/tools
 bash parse_with_turboparser.sh $file.tgt $model
 cd -
 python parsed_to_inline_notation.py --infile $file.tgt.parsed > $file.tgt.inline
-rm $file.tgt
