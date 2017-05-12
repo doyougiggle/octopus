16827fe3572abfc79569826e2be8bc8f1dff4105
hectormartinez <hector.martinez.a@gmail.com>
2017-05-13 00:14:19 +0200

    X

--- src/parse_all_train_de-en.sh
+++ src/parse_all_train_de-en.sh
@@ -7,8 +7,8 @@ target_mod=/projdata/alpage2/hmartine/tools/en_lemma_petrov.turbomodel_svm_mira/
 source_mod=/projdata/alpage2/hmartine/tools/de_form_upos.turbomodel_svm_mira/de_train_form_upos.conll.model
 source_lang=de
 
-for file in `$DIR/*withids`
+for file in `ls $DIR/*withids`
 do
 bash parse_target_lemmapetrov.sh $file $target_mod
-bash parse_target_lemmapetrov.sh $file $source_mod $source_lang
+bash parse_source_formpos.sh $file $source_mod $source_lang
 done
