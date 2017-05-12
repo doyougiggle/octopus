ca558c09a38c09223fd87709d305627d95536c22
hectormartinez <hector.martinez.a@gmail.com>
2017-05-13 00:41:39 +0200

    outfile

--- src/discodata_source_2marmot.py
+++ src/discodata_source_2marmot.py
@@ -3,12 +3,13 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="""Run the NN tagger""")
     parser.add_argument("--infile", help="")
+    parser.add_argument("--outfile", help="")
     parser.add_argument("--column", help="",default=2,type=int)
     args = parser.parse_args()
 
     #you	you|PRON	Woran denken Sie , wenn ich das Wort " Design " sage ?	What|PRON do|VERB REPLACE_2 think|VERB of|ADP when|ADV I|PRON say|VERB the|DET word|NOUN "|. design|NOUN "|. ?|.	0-0 1-3 2-2 4-5 5-6 6-8 7-9 8-10 9-11 10-12 11-7 12-13	2039
     #form  form|POS   Tokenized text in the Source   Token|POS for|POS Target|POS alignments_that_I_ignore INDEX
-    fout = open(args.infile+"src",mode="w",encoding="utf-8")
+    fout = open(args.outfile",mode="w",encoding="utf-8")
     for line in open(args.infile,encoding="utf-8").readlines():
         blocks= line.split("\t")
         forms = blocks[args.column]
--- src/parse_source_formpos.sh
+++ src/parse_source_formpos.sh
@@ -3,11 +3,11 @@ file=$1
 model=$2
 lang=$3
 
-python discodata_source_2marmot.py --infile $file #> $file.src
+python discodata_source_2marmot.py --infile $file --outfile  $file.src  #> $file.src
 cd /projdata/alpage2/hmartine/tools/marmot
 bash tag_with_marmot.sh  $lang $file.src
 cd -
-python marmot2turboparser.py --infile $file.src.pos #> $file.src2
+python marmot2turboparser.py --infile $file.src.pos --outfile  $file.src2
 cd /projdata/alpage2/hmartine/tools
 bash parse_with_turboparser.sh $file.src2 $model
 cd -
